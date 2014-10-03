6c64364ca50e07b86e658082242cd42402816d51
Alexander Nefedov <nefedov.alexander91@yandex.ru>
2014-10-04 04:55:13 +0600

    [20] AnAbstractBuilding class is implemented.

---
+++ core/__init__.py
@@ -0,0 +1,38 @@
+# coding: utf-8
+
+from __future__     import division
+from math           import modf
+
+class AnAbstractBuilding(object):
+    """docstring for AnAbstractBuilding"""
+    __slots__       = ( "__complexity", "__cost", "__progress" )
+    def __init__( self, complexity=0, cost=0 ):
+        super( AnAbstractBuilding, self ).__init__()
+        self.__cost         = cost
+        self.__complexity   = complexity
+        self.__progress     = 0 # Takes values from 0 to 1
+
+    @property 
+    def complexity( self ):
+        return self.__complexity
+
+    @property 
+    def cost( self ):
+        return self.__cost
+
+    @property 
+    def progress( self ):
+        return round( self.__progress * 100, 2 )
+
+    def Build( self, building_modules ):
+        self.__cost += building_modules / self.__complexity
+        if self.__cost > 1:
+            self.__cost = 1
+
+    def CalculateBuildingPeriod( self, building_modules ):
+        period  = ( self.__complexity * ( 1 - self.__progress ) ) / building_modules
+        fract   = modf( period )
+        if fract[ 0 ] != 0:
+            return int( period + 1 )
+        else:
+            return int( period )
