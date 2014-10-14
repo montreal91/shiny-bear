5597c3b71ef6067c83a9f6aa7f62da62c4931d3f
Alexander Nefedov <nefedov.alexander91@yandex.ru>
2014-10-15 01:46:32 +0600

    [47] Added status codes

--- core/code_constants.py
+++ core/code_constants.py
@@ -1,14 +1,20 @@
 # coding: utf-8
 
-PRECISION   = 2
+from helpers import AStruct
 
-RECRUIT     = 0
-SOLDIER     = 1
-CAPTAIN     = 2
-GENERAL     = 3
+PRECISION               = 2
 
-DISCIPLINE  = "dis"
-ATTACK      = "att"
-DEFENCE     = "def"
-LOGISTICS   = "log"
-LEADERSHIP  = "lead"
+RECRUIT                 = 0
+SOLDIER                 = 1
+CAPTAIN                 = 2
+GENERAL                 = 3
+
+DISCIPLINE              = "dis"
+ATTACK                  = "att"
+DEFENCE                 = "def"
+LOGISTICS               = "log"
+LEADERSHIP              = "lead"
+
+STATUS_CODES            = AStruct()
+STATUS_CODES.SUCCESS    = True
+STATUS_CODES.FAILURE    = False
